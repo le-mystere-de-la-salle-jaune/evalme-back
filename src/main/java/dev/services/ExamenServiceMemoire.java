b65581c3d561ceb6e0debf1bd8334731d38b3199
joachim <joachim.vanoni@gmail.com>
2018-07-09 17:15:42 +0200

    commit fix erreur utlisateurService

--- src/main/java/dev/services/ExamenServiceMemoire.java
+++ src/main/java/dev/services/ExamenServiceMemoire.java
@@ -1,16 +1,11 @@
 package dev.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import dev.entites.Examen;
 
 @Service
 public class ExamenServiceMemoire extends ServiceMemoire {
-
-	private List<Examen> examens = new ArrayList<>();
 
 	public ExamenServiceMemoire() {
 		super("jdd/jdd-examens.xml", Examen.class);
--- src/main/java/dev/services/UtilisateurServiceMemoire.java
+++ src/main/java/dev/services/UtilisateurServiceMemoire.java
@@ -1,8 +1,5 @@
 package dev.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import dev.entites.Utilisateur;
@@ -10,10 +7,8 @@ import dev.entites.Utilisateur;
 @Service
 public class UtilisateurServiceMemoire extends ServiceMemoire {
 
-	private List<Utilisateur> utilisateurs = new ArrayList<>();
-
 	public UtilisateurServiceMemoire() {
-		super("jdd/jdd-utilisateur.xml", UtilisateurServiceMemoire.class);
+		super("jdd/jdd-utilisateur.xml", Utilisateur.class);
 	}
 
 }
