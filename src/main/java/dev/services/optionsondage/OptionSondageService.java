dca53454b5b571d88eeb9e349cf0f35820d08eae
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-07-09 17:17:51 +0200

    ajout OptionSondageServiceMemoire

---
+++ src/main/java/dev/services/optionsondage/OptionSondageService.java
@@ -0,0 +1,8 @@
+package dev.services.optionsondage;
+
+import dev.entites.OptionSondage;
+import dev.services.CrudService;
+
+public interface OptionSondageService extends CrudService<OptionSondage> {
+
+}
