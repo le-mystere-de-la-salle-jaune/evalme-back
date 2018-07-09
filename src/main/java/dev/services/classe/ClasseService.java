a520d9cb9c99a7b2a841ed2c731242ed3f119304
TREMION Maxime <maxime.tremion@hotmail.fr>
2018-07-09 17:00:03 +0200

    Creatation service/serviceMemoire/testServiceMemoire

---
+++ src/main/java/dev/services/classe/ClasseService.java
@@ -0,0 +1,8 @@
+package dev.services.classe;
+
+import dev.entites.Classe;
+import dev.services.CrudService;
+
+public interface ClasseService extends CrudService<Classe> {
+
+}
---
+++ src/test/java/dev/services/ClasseServiceMemoireTest.java
@@ -0,0 +1,70 @@
+package dev.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import dev.entites.Classe;
+import dev.services.classe.ClasseServiceMemoire;
+
+@ContextConfiguration(classes = { ClasseServiceMemoire.class })
+@RunWith(SpringRunner.class)
+public class ClasseServiceMemoireTest {
+
+	@Autowired
+	public ClasseServiceMemoire classeServiceMemoire = new ClasseServiceMemoire();
+
+	@Test
+	public void TestfindAll() {
+		// TODO Auto-generated method stub
+		List<Classe> classe = classeServiceMemoire.findAll();
+
+		assertThat(classe).isNotEmpty();
+		assertThat(classe.size()).isEqualTo(3);
+		assertThat(classe.get(0).getStagiaires().get(0).getPrenom()).isEqualTo("Maxime");
+	}
+
+	@Test
+	public void TestSave() {
+		// TODO Auto-generated method stub
+		List<Classe> classe = classeServiceMemoire.findAll();
+		Classe c = classe.get(0);
+		c.setId(4L);
+
+		classe.add(c);
+
+		assertThat(classe.size()).isEqualTo(4);
+	}
+
+	@Test
+	public void TestUpdate() {
+		// TODO Auto-generated method stub
+		List<Classe> classe = classeServiceMemoire.findAll();
+		Classe c = classe.get(0);
+
+		c.setNom("D21");
+
+		classeServiceMemoire.update(c);
+
+		assertThat(classe.get(0).getNom()).isEqualTo("D21");
+	}
+
+	@Test
+	public void TestDelete() {
+		// TODO Auto-generated method stub
+		List<Classe> classe = classeServiceMemoire.findAll();
+		Classe c = classe.get(2);
+
+		classe.remove(c);
+
+		assertThat(classe.size()).isEqualTo(2);
+
+	}
+
+}
