12e1478cf31b73a2bc6e549b8a64ac6fbdf7d7e8
joachim <joachim.vanoni@gmail.com>
2018-07-09 12:52:46 +0200

    commit ajout des examens

--- src/main/java/dev/entites/Note.java
+++ src/main/java/dev/entites/Note.java
@@ -2,22 +2,32 @@ package dev.entites;
 
 public class Note extends BaseEntite {
 
-    private Stagiaire stagiaire;
-    private Float noteSur20;
+	private Stagiaire stagiaire;
+	private Float noteSur20;
 
-    public Stagiaire getStagiaire() {
-        return stagiaire;
-    }
+	public Note() {
 
-    public void setStagiaire(Stagiaire stagiaire) {
-        this.stagiaire = stagiaire;
-    }
+	}
 
-    public Float getNoteSur20() {
-        return noteSur20;
-    }
+	public Note(Stagiaire stagiaire, Float noteSur20) {
+		super();
+		this.stagiaire = stagiaire;
+		this.noteSur20 = noteSur20;
+	}
 
-    public void setNoteSur20(Float noteSur20) {
-        this.noteSur20 = noteSur20;
-    }
+	public Stagiaire getStagiaire() {
+		return stagiaire;
+	}
+
+	public void setStagiaire(Stagiaire stagiaire) {
+		this.stagiaire = stagiaire;
+	}
+
+	public Float getNoteSur20() {
+		return noteSur20;
+	}
+
+	public void setNoteSur20(Float noteSur20) {
+		this.noteSur20 = noteSur20;
+	}
 }
