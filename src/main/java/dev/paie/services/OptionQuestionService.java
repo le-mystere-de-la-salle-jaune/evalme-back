e1fca73a273549335cb3494c68e85d78c9b1edcb
Redor <matthieu.redor@hotmail.fr>
2018-07-10 10:11:46 +0200

    Ajout Service et Test des Questions et OptionQuestions

---
+++ src/main/java/dev/paie/services/OptionQuestionService.java
@@ -0,0 +1,11 @@
+package dev.paie.services;
+
+import org.springframework.stereotype.Component;
+
+import dev.entites.OptionQuestion;
+import dev.services.CrudService;
+
+@Component
+public interface OptionQuestionService extends CrudService<OptionQuestion> {
+
+}
