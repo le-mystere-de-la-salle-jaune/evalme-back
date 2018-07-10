32d94ef1489a78cb878e80ff79c893eeaf90dd9e
Redor <matthieu.redor@hotmail.fr>
2018-07-10 17:50:28 +0200

    Commencement du formulaire de création des options pour les questions

--- src/main/java/dev/entites/OptionQuestion.java
+++ src/main/java/dev/entites/OptionQuestion.java
@@ -1,10 +1,17 @@
 package dev.entites;
 
+import javax.validation.constraints.NotEmpty;
+
 public class OptionQuestion extends BaseEntite {
 
+	@NotEmpty
 	private String libelle;
-
+	@NotEmpty
 	private Boolean ok;
+
+	public OptionQuestion() {
+		super();
+	}
 
 	public String getLibelle() {
 		return libelle;
