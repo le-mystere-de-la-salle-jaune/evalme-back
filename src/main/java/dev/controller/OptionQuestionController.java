7ca099f79461f095bb11dd69e313e871e28b7c87
Redor <matthieu.redor@hotmail.fr>
2018-07-10 17:50:28 +0200

    Commencement du formulaire de création des options pour les questions

--- src/main/java/dev/controller/OptionQuestionController.java
+++ src/main/java/dev/controller/OptionQuestionController.java
@@ -2,9 +2,12 @@ package dev.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import dev.entites.OptionQuestion;
 import dev.metiers.OptionQuestionService;
 
 @Controller
@@ -23,6 +26,26 @@ public class OptionQuestionController {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("listeOptionQuestions", optionQuestionService.lister());
 		mv.setViewName("questions/listerOptionQuestions");
+		return mv;
+	}
+
+	@GetMapping("/creerOption")
+	public ModelAndView creerOption() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("questions/creerOptionQuestions");
+		return mv;
+	}
+
+	@PostMapping("/creerOption")
+	public ModelAndView postCreerOption(@RequestParam String id, @RequestParam String libelle,
+			@RequestParam String ok) {
+		OptionQuestion newOption = new OptionQuestion();
+		newOption.setId(Long.parseLong(id));
+		newOption.setLibelle(libelle);
+		newOption.setOk(Boolean.valueOf(ok));
+		optionQuestionService.lister().add(newOption);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("questions/creerOptionQuestions");
 		return mv;
 	}
 
