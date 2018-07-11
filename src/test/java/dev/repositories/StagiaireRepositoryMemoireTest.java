65aff4523a908b3b9150c36e093fbc239b47d5f8
joachim <joachim.vanoni@gmail.com>
2018-07-11 15:05:31 +0200

    modification Junit Passant

--- src/test/java/dev/repositories/StagiaireRepositoryMemoireTest.java
+++ src/test/java/dev/repositories/StagiaireRepositoryMemoireTest.java
@@ -26,15 +26,15 @@ public class StagiaireRepositoryMemoireTest {
 	@Test
 	public void testFindAll() {
 		List<Stagiaire> stagiaires = service.findAll();
-		assertThat(stagiaires.size()).isEqualTo(3);
+		assertThat(stagiaires.size()).isEqualTo(8);
 
 	}
 
 	@Test
 	public void testSave() {
-		assertThat(service.findAll().size()).isEqualTo(3);
+		assertThat(service.findAll().size()).isEqualTo(8);
 		service.save(new Stagiaire());
-		assertThat(service.findAll().size()).isEqualTo(4);
+		assertThat(service.findAll().size()).isEqualTo(9);
 	}
 
 	@Test
@@ -49,10 +49,10 @@ public class StagiaireRepositoryMemoireTest {
 
 	@Test
 	public void testDelete() {
-		assertThat(service.findAll().size()).isEqualTo(4);
+		assertThat(service.findAll().size()).isEqualTo(9);
 		Stagiaire s = service.findAll().get(0);
 		service.delete(s);
-		assertThat(service.findAll().size()).isEqualTo(3);
+		assertThat(service.findAll().size()).isEqualTo(8);
 	}
 
 	// tester toutes les méthodes du service
