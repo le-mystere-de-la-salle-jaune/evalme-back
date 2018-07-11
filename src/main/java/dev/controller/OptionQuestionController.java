7bb8bcabfdde79a1354325a1a396f786bdfd4d0e
Redor <matthieu.redor@hotmail.fr>
2018-07-11 17:48:42 +0200

    Suppression des Questions et OptionQuestions

--- src/main/java/dev/controller/OptionQuestionController.java
+++ src/main/java/dev/controller/OptionQuestionController.java
@@ -12,17 +12,35 @@ import org.springframework.web.servlet.ModelAndView;
 import dev.entites.OptionQuestion;
 import dev.metiers.OptionQuestionService;
 
+/**
+ * OptionQuestionController.java
+ *
+ * @author matth
+ */
 @Controller
 @RequestMapping("/questions")
 public class OptionQuestionController {
 
+	/**
+	 * optionQuestionService : OptionQuestionService
+	 */
 	private OptionQuestionService optionQuestionService;
 
+	/**
+	 * Constructor of OptionQuestionController.java
+	 *
+	 * @param optionQuestionService
+	 */
 	public OptionQuestionController(OptionQuestionService optionQuestionService) {
 		super();
 		this.optionQuestionService = optionQuestionService;
 	}
 
+	/**
+	 * Fonction : Lister les options avec une JSP
+	 *
+	 * @return
+	 */
 	@GetMapping("/listerOption")
 	public ModelAndView lister() {
 		ModelAndView mv = new ModelAndView();
@@ -31,6 +49,11 @@ public class OptionQuestionController {
 		return mv;
 	}
 
+	/**
+	 * Fonction : Afficher la création d'une option avec une JSP
+	 *
+	 * @return
+	 */
 	@GetMapping("/creerOption")
 	public ModelAndView creerOption() {
 		ModelAndView mv = new ModelAndView();
@@ -39,6 +62,13 @@ public class OptionQuestionController {
 		return mv;
 	}
 
+	/**
+	 * Fonction : Créer une option avec une JSP
+	 *
+	 * @param option
+	 * @param result
+	 * @return
+	 */
 	@PostMapping("/creerOption")
 	public ModelAndView postCreerOption(@ModelAttribute("option") OptionQuestion option, BindingResult result) {
 		optionQuestionService.getOptionQuestionRepository().save(option);
@@ -47,6 +77,11 @@ public class OptionQuestionController {
 		return mv;
 	}
 
+	/**
+	 * Fonction : Afficher la mise à jour d'une option avec une JSP
+	 *
+	 * @return
+	 */
 	@GetMapping("/majOption")
 	public ModelAndView majOption() {
 		ModelAndView mv = new ModelAndView();
@@ -56,6 +91,14 @@ public class OptionQuestionController {
 		return mv;
 	}
 
+	/**
+	 * Fonction : Mettre à jour une option avec une JSP
+	 *
+	 * @param option
+	 * @param choix
+	 * @param result
+	 * @return
+	 */
 	@PostMapping("/majOption")
 	public ModelAndView postMajOption(@ModelAttribute("option") OptionQuestion option, @RequestParam String choix,
 			BindingResult result) {
@@ -64,6 +107,34 @@ public class OptionQuestionController {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("listeOptionQuestions", optionQuestionService.lister());
 		mv.setViewName("questions/majOptionQuestions");
+		return mv;
+	}
+
+	/**
+	 * Fonction : Afficher la suppression d'une question avec une JSP
+	 *
+	 * @return
+	 */
+	@GetMapping("/supprimerOption")
+	public ModelAndView supprimerOption() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("listeOptionQuestions", optionQuestionService.lister());
+		mv.setViewName("questions/supprimerOptionQuestions");
+		return mv;
+	}
+
+	/**
+	 * Fonction : Supprimer une question avec une JSP
+	 *
+	 * @param choix
+	 * @return
+	 */
+	@PostMapping("/supprimerOption")
+	public ModelAndView postSupprimerOption(@RequestParam String choix) {
+		optionQuestionService.getOptionQuestionRepository().delete(optionQuestionService.trouverAvecId(choix));
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("listeOptionQuestions", optionQuestionService.lister());
+		mv.setViewName("questions/supprimerOptionQuestions");
 		return mv;
 	}
 
