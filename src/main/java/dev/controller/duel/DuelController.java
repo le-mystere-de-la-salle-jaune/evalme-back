9fdf3db7e3e7f427eb219fad2ddd7f1029f604c1
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-07-12 11:37:16 +0200

    WIP suppression duel

--- src/main/java/dev/controller/duel/DuelController.java
+++ src/main/java/dev/controller/duel/DuelController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -162,7 +161,7 @@ public class DuelController {
 	 * @param id
 	 * @return
 	 */
-	@DeleteMapping("/duels/editer")
+	@PostMapping("/duels/supprimer")
 	public ModelAndView supprimer(@RequestParam("id") Long id) {
 		ModelAndView mav = new ModelAndView();
 		Duel duel = duelService.getById(id);
