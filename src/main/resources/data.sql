1bc05b900f3e775da836f25d3c74793794d24f53
TREMION Maxime <maxime.tremion@hotmail.fr>
2018-07-13 15:28:53 +0200

    correction

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,29 @@
-insert into classe(nom) values ('D11');
 
-insert into classe(nom) values ('D12');
 
-insert into classe(nom) values ('D13');
+INSERT INTO classe ( nom ) values ('D11');
+INSERT INTO classe ( nom ) values ('D12');
+INSERT INTO classe ( nom ) values ('D13');
+INSERT INTO classe ( nom ) values ('D14');
+
+
+
+insert into stagiaire(nom, prenom, email, photo_url) values ('Abitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Bbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Cbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Dbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Ebitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Fbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Gbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Hbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+
+insert into stagiaire(nom, prenom, email, photo_url) values ('Ibitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Jbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Kbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Lbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Mbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Nbitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+insert into stagiaire(nom, prenom, email, photo_url) values ('Obitbol', 'Georges', 'mondedemerde@clas.se', 'abitbol.jpg');
+
+
+
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,32 @@
-drop table if exists classe;
 
-create table classe(
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table if exists classe;
+drop table if exists stagiaire;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+CREATE TABLE classe (
   id serial primary key,
-  nom varchar(75) not null
+  nom varchar(20) NOT NULL
 );
+
+create table stagiaire (
+  
+	id serial primary key,
+  
+	nom varchar(75) not null,
+  
+	prenom varchar(75) not null,
+  
+	email varchar(75) not null,
+  
+	photo_url varchar(200) not null
+,  
+	id_classe BIGINT UNSIGNED,
+  
+	FOREIGN KEY (id_classe) REFERENCES classe(id)
+
+);
+
+
