d8daf9583e7a09b9aec5f86b6118aabdbe0dbedc
Corentin Torres <torres.corentin@gmail.com>
2018-07-13 10:39:38 +0200

    Correction des fichiers data.sql et schema.sql

--- src/main/java/dev/repositories/quizz/QuizzRepositoryJdbc.java
+++ src/main/java/dev/repositories/quizz/QuizzRepositoryJdbc.java
@@ -3,13 +3,30 @@ package dev.repositories.quizz;
 import java.util.List;
 import java.util.Optional;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import dev.entites.Quizz;
 
+@Repository
 public class QuizzRepositoryJdbc implements QuizzRepository {
+
+	// outil JdbcTemplate fourni par Spring JDBC
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired // injection de la source de données
+	public QuizzRepositoryJdbc(DataSource datasource) {
+		this.jdbcTemplate = new JdbcTemplate(datasource);
+	}
 
 	@Override
 	public List<Quizz> findAll() {
-		// TODO Auto-generated method stub
+		String sql = "SELECT * FROM quizz";
+		// exécution de la requête et récupération du résultat
+		// this.jdbcTemplate.query(sql, new QuizzMapper())
 		return null;
 	}
 
--- src/main/java/dev/repositories/quizz/QuizzRepositoryMemoire.java
+++ src/main/java/dev/repositories/quizz/QuizzRepositoryMemoire.java
@@ -3,12 +3,10 @@ package dev.repositories.quizz;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
 import dev.entites.Quizz;
 import dev.repositories.BaseRepositoryMemoire;
 
-@Service
 public class QuizzRepositoryMemoire extends BaseRepositoryMemoire<Quizz> implements QuizzRepository {
 
 	public QuizzRepositoryMemoire(@Value("${jdd.quizz}") String path) {
