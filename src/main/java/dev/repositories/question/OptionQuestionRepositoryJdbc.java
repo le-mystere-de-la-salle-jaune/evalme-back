75ea568545f84241af48fec08396becc1ba0d55b
Redor <matthieu.redor@hotmail.fr>
2018-07-13 10:56:39 +0200

    Partie JDBC avec la fonction findAll fonctionnelle

---
+++ src/main/java/dev/repositories/question/OptionQuestionRepositoryJdbc.java
@@ -0,0 +1,52 @@
+package dev.repositories.question;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import dev.entites.OptionQuestion;
+
+@Repository
+public class OptionQuestionRepositoryJdbc implements OptionQuestionRepository {
+
+	// outil JdbcTemplate fourni par Spring JDBC
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired // injection de la source de données
+	public OptionQuestionRepositoryJdbc(DataSource datasource) {
+		this.jdbcTemplate = new JdbcTemplate(datasource);
+	}
+
+	@Override
+	public List<OptionQuestion> findAll() {
+		String sql = "SELECT * FROM OPTION_QUESTION";
+		List<OptionQuestion> optionQuestions = jdbcTemplate.query(sql, new OptionQuestionMapper());
+		return optionQuestions;
+	}
+
+	@Override
+	public void save(OptionQuestion entite) {
+
+	}
+
+	@Override
+	public void update(OptionQuestion entiteAvecId) {
+
+	}
+
+	@Override
+	public void delete(OptionQuestion entite) {
+
+	}
+
+	@Override
+	public Optional<OptionQuestion> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
