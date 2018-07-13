697d1c368a3da613e4b19ec6c55be69821b31819
joachim <joachim.vanoni@gmail.com>
2018-07-13 15:49:01 +0200

    commit finla, drop table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,50 +1,50 @@
-INSERT INTO classe ( nom ) values ('D11');
-INSERT INTO classe ( nom ) values ('D12');
-INSERT INTO classe ( nom ) values ('D13');
-INSERT INTO classe ( nom ) values ('D14');
+insert into classe ( nom ) values ('d11');
+insert into classe ( nom ) values ('d12');
+insert into classe ( nom ) values ('d13');
+insert into classe ( nom ) values ('d14');
 
 insert into stagiaire(nom, prenom, email, photo_url, id_classe)
-  values ('Clopin', 'Brigitte', 'bribri@hotmail.fr', 'https://www.valeursactuelles.com/sites/default/files/styles/image_article/public/2018-01/brigitte%20macron%20sipa.jpg?itok=0G8jRPfF', 1);
+  values ('clopin', 'brigitte', 'bribri@hotmail.fr', 'https://www.valeursactuelles.com/sites/default/files/styles/image_article/public/2018-01/brigitte%20macron%20sipa.jpg?itok=0g8jrpff', 1);
 
 insert into stagiaire(nom, prenom, email, photo_url, id_classe)
-  values ('Lupin', 'Patrick', 'lp@gmail.com', 'https://odgers-prod-neu-cdn-tm-endpoint.azureedge.net/media/1351/patrik-kvikant.jpg',1 );
+  values ('lupin', 'patrick', 'lp@gmail.com', 'https://odgers-prod-neu-cdn-tm-endpoint.azureedge.net/media/1351/patrik-kvikant.jpg',1 );
   
 insert into stagiaire(nom, prenom, email, photo_url, id_classe)
-  values ('Lupin', 'Patrick', 'lp@gmail.com', 'https://odgers-prod-neu-cdn-tm-endpoint.azureedge.net/media/1351/patrik-kvikant.jpg',2 );
+  values ('lupin', 'patrick', 'lp@gmail.com', 'https://odgers-prod-neu-cdn-tm-endpoint.azureedge.net/media/1351/patrik-kvikant.jpg',2 );
   
 insert into stagiaire(nom, prenom, email, photo_url, id_classe)
-  values ('Lupin', 'Patrick', 'lp@gmail.com', 'https://odgers-prod-neu-cdn-tm-endpoint.azureedge.net/media/1351/patrik-kvikant.jpg',2 );
+  values ('lupin', 'patrick', 'lp@gmail.com', 'https://odgers-prod-neu-cdn-tm-endpoint.azureedge.net/media/1351/patrik-kvikant.jpg',2 );
  
-insert into QUIZZ(titre) values ('Les animeaux de la forêt');
+insert into quizz(titre) values ('les animeaux de la forêt');
 
-insert into EXAMEN(titre,id_quizz,id_classe) values ('Quiz D12 animeaux de la forêts', 1, 1) ;
+insert into examen(titre,id_quizz,id_classe) values ('quiz d12 animeaux de la forêts', 1, 1) ;
 
-insert into NOTE(note_sur_20, id_stagiaire, id_examen) values (12, 3, 1);
-insert into NOTE(note_sur_20, id_stagiaire, id_examen) values (15, 4, 1);
+insert into note(note_sur_20, id_stagiaire, id_examen) values (12, 3, 1);
+insert into note(note_sur_20, id_stagiaire, id_examen) values (15, 4, 1);
 
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 1','description du sondage numéro 1');
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 2','description du sondage numéro 2');
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 3','description du sondage numéro 3');
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 4','description du sondage numéro 4');
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 5','description du sondage numéro 5');
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 6','description du sondage numéro 6');
-INSERT INTO option_sondage(libelle, description) VALUES ('libelle sondage 7','description du sondage numéro 7');
+insert into option_sondage(libelle, description) values ('libelle sondage 1','description du sondage numéro 1');
+insert into option_sondage(libelle, description) values ('libelle sondage 2','description du sondage numéro 2');
+insert into option_sondage(libelle, description) values ('libelle sondage 3','description du sondage numéro 3');
+insert into option_sondage(libelle, description) values ('libelle sondage 4','description du sondage numéro 4');
+insert into option_sondage(libelle, description) values ('libelle sondage 5','description du sondage numéro 5');
+insert into option_sondage(libelle, description) values ('libelle sondage 6','description du sondage numéro 6');
+insert into option_sondage(libelle, description) values ('libelle sondage 7','description du sondage numéro 7');
 
-INSERT INTO sondage(classe_id) VALUES (1);
-INSERT INTO sondage(classe_id) VALUES (2);
-INSERT INTO sondage(classe_id) VALUES (3);
-INSERT INTO sondage(classe_id) VALUES (1);
+insert into sondage(classe_id) values (1);
+insert into sondage(classe_id) values (2);
+insert into sondage(classe_id) values (3);
+insert into sondage(classe_id) values (1);
 
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (1,3);
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (1,5);
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (1,7);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (1,3);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (1,5);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (1,7);
 
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (2,1);
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (2,4);
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (2,7);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (2,1);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (2,4);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (2,7);
 
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (3,2);
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (3,6);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (3,2);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (3,6);
 
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (4,1);
-INSERT INTO sondage_option_sondage ( id_sondage, id_option_sondage ) VALUES (4,7);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (4,1);
+insert into sondage_option_sondage ( id_sondage, id_option_sondage ) values (4,7);
--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,21 +1,20 @@
 drop table if exists sondage_option_sondage;
 drop table if exists question_compo;
-drop table if exists Sondage;
+drop table if exists sondage;
 drop table if exists option_sondage;
-drop table if exists NOTE;
-drop table if exists EXAMEN;
-DROP TABLE IF EXISTS compo_quizz;
-drop table if exists QUIZZ;
-drop table if exists QUESTION_COMPO
-drop table if exists OPTION_QUESTION;
-drop table if exists QUESTION;
+drop table if exists note;
+drop table if exists examen;
+drop table if exists compo_quizz;
+drop table if exists quizz;
+drop table if exists option_question;
+drop table if exists question;
 drop table if exists stagiaire;
 drop table if exists classe;
 
 
-CREATE TABLE classe (
+create table classe (
   id serial primary key,
-  nom varchar(75) NOT NULL
+  nom varchar(75) not null
 );
 
 create table stagiaire (
@@ -24,76 +23,76 @@ create table stagiaire (
   prenom varchar(75) not null,
   email varchar(75) not null,
   photo_url varchar(200) not null,
-  id_classe BIGINT not null,
-  FOREIGN KEY (id_classe) REFERENCES classe(id)
+  id_classe bigint not null,
+  foreign key (id_classe) references classe(id)
 );
 
-create table QUESTION (
+create table question (
   id serial primary key,
-  titre VARCHAR(75)
+  titre varchar(75)
 );
 
-create table OPTION_QUESTION (
+create table option_question (
   id serial primary key,
-  libelle VARCHAR(75) not null,
-  ok BIT(1) not null
+  libelle varchar(75) not null,
+  ok bit(1) not null
 );
 
-create table QUESTION_COMPO (
+create table question_compo (
   id serial primary key,
-  id_opt BIGINT not null,
-  id_que BIGINT not null,
-  FOREIGN KEY (id_opt) REFERENCES OPTION_QUESTION(id),
-  FOREIGN KEY (id_que) REFERENCES QUESTION(id)
+  id_opt bigint not null,
+  id_que bigint not null,
+  foreign key (id_opt) references option_question(id),
+  foreign key (id_que) references question(id)
 );
 
-create table QUIZZ(
+create table quizz(
 	id serial primary key,
 	titre varchar(75) not null
 );
 
-CREATE TABLE compo_quizz (
-	id SERIAL PRIMARY KEY,
-	idQuizz BIGINT,
-	idQuestion BIGINT,
-	FOREIGN KEY (idQuizz) REFERENCES QUIZZ(id),
-	FOREIGN KEY (idQuestion) REFERENCES QUESTION(id)
-);
-
-CREATE TABLE EXAMEN(
+create table compo_quizz (
 	id serial primary key,
-	titre VARCHAR(75) not null,
-	id_quizz BIGINT not null,
-	id_classe BIGINT not null,
-	FOREIGN KEY (id_quizz) REFERENCES QUIZZ(id),
-	FOREIGN KEY (id_classe) REFERENCES classe(id)
+	idquizz bigint,
+	idquestion bigint,
+	foreign key (idquizz) references quizz(id),
+	foreign key (idquestion) references question(id)
+);
+
+create table examen(
+	id serial primary key,
+	titre varchar(75) not null,
+	id_quizz bigint not null,
+	id_classe bigint not null,
+	foreign key (id_quizz) references quizz(id),
+	foreign key (id_classe) references classe(id)
 );
 
 
-create table NOTE (
+create table note (
   id serial primary key,
-  note_sur_20 DECIMAL(4,2) not null,
-  id_stagiaire BIGINT not null,
-  id_examen BIGINT not null,
-  FOREIGN KEY (id_stagiaire) REFERENCES stagiaire(id),
-  FOREIGN KEY (id_examen) REFERENCES EXAMEN(id)
+  note_sur_20 decimal(4,2) not null,
+  id_stagiaire bigint not null,
+  id_examen bigint not null,
+  foreign key (id_stagiaire) references stagiaire(id),
+  foreign key (id_examen) references examen(id)
 );
 
-CREATE TABLE option_sondage (
+create table option_sondage (
   id serial primary key,
-  libelle varchar(75) NOT NULL,
-  description varchar(150) NOT NULL
+  libelle varchar(75) not null,
+  description varchar(150) not null
 );
 
-CREATE TABLE sondage (
+create table sondage (
   id serial primary key,
-  classe_id bigint DEFAULT NULL,
-  FOREIGN KEY (classe_id) REFERENCES classe (id)
+  classe_id bigint default null,
+  foreign key (classe_id) references classe (id)
 );
 
-CREATE TABLE sondage_option_sondage (
-  id_sondage bigint DEFAULT NULL,
-  id_option_sondage bigint DEFAULT NULL,
-  FOREIGN KEY (id_option_sondage) REFERENCES option_sondage (id),
-  FOREIGN KEY (id_sondage) REFERENCES sondage (id)
+create table sondage_option_sondage (
+  id_sondage bigint default null,
+  id_option_sondage bigint default null,
+  foreign key (id_option_sondage) references option_sondage (id),
+  foreign key (id_sondage) references sondage (id)
 );
