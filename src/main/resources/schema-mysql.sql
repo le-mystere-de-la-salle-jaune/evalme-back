4859d22fa0d55fdd000852702a4c5532579e3bd3
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-07-16 12:17:22 +0200

    fix

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -111,23 +111,3 @@ create table duel (
 	foreign key (quizz_id) references quizz(id)
 );
 
-
-CREATE TABLE option_sondage (
-  id serial primary key,
-  libelle varchar(75) NOT NULL,
-  description varchar(150) NOT NULL
-);
-
-CREATE TABLE sondage (
-  id serial primary key,
-  classe_id bigint(20) UNSIGNED DEFAULT NULL,
-  FOREIGN KEY (classe_id) REFERENCES classe (id)
-);
-
-CREATE TABLE sondage_option_sondage (
-  id_sondage bigint(20) UNSIGNED DEFAULT NULL,
-  id_option_sondage bigint(20) UNSIGNED DEFAULT NULL,
-  FOREIGN KEY (id_option_sondage) REFERENCES option_sondage (id),
-  FOREIGN KEY (id_sondage) REFERENCES sondage (id)
-);
-
