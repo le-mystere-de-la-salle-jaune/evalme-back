afe96ed04e6b293cafa4af0ca4a603e6ce476fa3
TREMION Maxime <maxime.tremion@hotmail.fr>
2018-07-16 16:17:56 +0200

    Test avec DataJPA opérationnel correction V2

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -12,6 +12,6 @@ create table stagiaire (
   prenom varchar(75) not null,
   email varchar(75) not null,
   photo_url varchar(200) not null,
-  id_classe bigint,
+  id_classe bigint unsigned,
   foreign key (id_classe) references classe(id)
 );
