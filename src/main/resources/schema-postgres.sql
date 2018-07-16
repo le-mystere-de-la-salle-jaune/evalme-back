2d3b30efa96166f816f13a39d92f58d4c3d4b365
Redor <matthieu.redor@hotmail.fr>
2018-07-16 15:12:27 +0200

    Data Jpa OptionQuestion - Correctif Heroku (2)

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -96,19 +96,6 @@ create table sondage_option_sondage (
   foreign key (id_sondage) references sondage (id)
 );
 
-CREATE TABLE sondage (
-  id serial primary key,
-  classe_id bigint,
-  FOREIGN KEY (classe_id) REFERENCES classe (id)
-);
-
-CREATE TABLE sondage_option_sondage (
-  id_sondage bigint,
-  id_option_sondage bigint,
-  FOREIGN KEY (id_option_sondage) REFERENCES option_sondage (id),
-  FOREIGN KEY (id_sondage) REFERENCES sondage (id)
-);
-
 create table duel (
 	id serial primary key,
 	stagiairea_id bigint not null,
