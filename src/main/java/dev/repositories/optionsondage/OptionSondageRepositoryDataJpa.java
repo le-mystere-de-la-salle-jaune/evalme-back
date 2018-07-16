e15e7cdbc78298886793670981b79115fc1901de
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-07-16 14:10:06 +0200

    spring data optionSondage

--- src/main/java/dev/repositories/optionsondage/OptionSondageRepositoryDataJpa.java
+++ src/main/java/dev/repositories/optionsondage/OptionSondageRepositoryDataJpa.java
@@ -3,14 +3,9 @@ package dev.repositories.optionsondage;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-
 import dev.entites.OptionSondage;
 
-public class OptionSondageRepositoryDataJpa implements OptionSondageDataJpaRepository {
+public class OptionSondageRepositoryDataJpa implements OptionSondageRepository {
 
 	private OptionSondageDataJpaRepository entiteDataJpa;
 
@@ -20,145 +15,29 @@ public class OptionSondageRepositoryDataJpa implements OptionSondageDataJpaRepos
 	}
 
 	@Override
-	public void deleteAllInBatch() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteInBatch(Iterable<OptionSondage> arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public List<OptionSondage> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return entiteDataJpa.findAll();
 	}
 
 	@Override
-	public List<OptionSondage> findAll(Sort arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public void save(OptionSondage entite) {
+		entiteDataJpa.save(entite);
 	}
 
 	@Override
-	public <S extends OptionSondage> List<S> findAll(Example<S> arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends OptionSondage> List<S> findAll(Example<S> arg0, Sort arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<OptionSondage> findAllById(Iterable<Long> arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void flush() {
+	public void update(OptionSondage entiteAvecId) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public OptionSondage getOne(Long arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends OptionSondage> List<S> saveAll(Iterable<S> arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends OptionSondage> S saveAndFlush(S arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<OptionSondage> findAll(Pageable arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void delete(OptionSondage arg0) {
+	public void delete(OptionSondage entite) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends OptionSondage> arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteById(Long arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean existsById(Long arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Optional<OptionSondage> findById(Long arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends OptionSondage> S save(S arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends OptionSondage> long count(Example<S> arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public <S extends OptionSondage> boolean exists(Example<S> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public <S extends OptionSondage> Page<S> findAll(Example<S> arg0, Pageable arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends OptionSondage> Optional<S> findOne(Example<S> arg0) {
+	public Optional<OptionSondage> findById(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/test/java/dev/repositories/optionSondage/OptionSondageRepositoryDataJpaTest.java
+++ src/test/java/dev/repositories/optionSondage/OptionSondageRepositoryDataJpaTest.java
@@ -30,16 +30,32 @@ public class OptionSondageRepositoryDataJpaTest {
 
 	@Test
 	public void testSave() {
+		int sizeInit = optionSondageRepositoryDataJpa.findAll().size();
+		OptionSondage option = new OptionSondage();
+		option.setId(15L);
+		option.setDescription("test");
+		option.setLibelle("test desc");
+		optionSondageRepositoryDataJpa.save(option);
+		assertThat(sizeInit).isNotEqualTo(optionSondageRepositoryDataJpa.findAll().size());
 
 	}
 
 	@Test
 	public void testUpdate() {
-
+		int sizeInit = optionSondageRepositoryDataJpa.findAll().size();
+		OptionSondage os = optionSondageRepositoryDataJpa.findAll().get(0);
+		assertThat(os.getLibelle()).isEqualTo("libelle sondage 2");
+		os.setLibelle("libelle test sondage 2");
+		optionSondageRepositoryDataJpa.save(os);
+		assertThat(optionSondageRepositoryDataJpa.findAll().get(0).getLibelle()).isEqualTo("libelle test sondage 2");
+		assertThat(sizeInit).isEqualTo(optionSondageRepositoryDataJpa.findAll().size());
 	}
 
 	@Test
 	public void testDelete() {
-
+		int sizeInit = optionSondageRepositoryDataJpa.findAll().size();
+		OptionSondage os = optionSondageRepositoryDataJpa.findAll().get(0);
+		optionSondageRepositoryDataJpa.delete(os);
+		assertThat(sizeInit).isGreaterThan(optionSondageRepositoryDataJpa.findAll().size());
 	}
 }
