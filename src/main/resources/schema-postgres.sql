5cb86249f9bf0190a7a64370617282e4cbf26717
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-07-17 10:29:02 +0200

    fix postgres

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -86,6 +86,7 @@ create table option_sondage (
 
 create table sondage (
   id serial primary key,
+  titre varchar(150) not null,
   classe_id bigint default null,
   foreign key (classe_id) references classe (id)
 );
