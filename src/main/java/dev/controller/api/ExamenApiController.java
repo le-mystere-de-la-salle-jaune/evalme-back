62cf3724b6c0610a8e454edc3e7ca1dca84056a9
Corentin Torres <torres.corentin@gmail.com>
2018-07-25 12:10:09 +0200

    Correction de conflits

--- src/main/java/dev/controller/api/ExamenApiController.java
+++ src/main/java/dev/controller/api/ExamenApiController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,6 @@ import dev.metiers.StagiaireService;
 
 @RestController
 @RequestMapping("/api/examens")
-@CrossOrigin
 public class ExamenApiController {
 
 	private ExamenService examenService;
